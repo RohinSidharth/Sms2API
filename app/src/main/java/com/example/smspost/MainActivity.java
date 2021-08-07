06c1c9d753048ac01a1576d782093ad19dc5c0fb
Rohin Sidharth <rohinsidharth@gmail.com>
2021-08-08 02:19:09 +0530

    POST
    Added code to POST and request permission to use WIFI to make the POST call. Pending test

--- app/src/main/java/com/example/smspost/MainActivity.java
+++ app/src/main/java/com/example/smspost/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
     //Ask for permissions
     private static final int MY_PERMISSIONS_REQUEST_RECEIVE_SMS = 0;
+    private static final int MY_PERMISSIONS_REQUEST_INTERNET = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +46,18 @@ public class MainActivity extends AppCompatActivity {
         switch(requestCode)
         {
             case MY_PERMISSIONS_REQUEST_RECEIVE_SMS:
+            {
+                //check if the length of grantResults is greater than 0 and is equal to PERMISSION_GRANTED
+                if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED)
+                {
+                    Toast.makeText(this, "Thanks for the Permissions", Toast.LENGTH_LONG).show();
+                }
+                else
+                {
+                    Toast.makeText(this, "App needs RECEIVE_SMS permission to continue", Toast.LENGTH_LONG).show();
+                }
+            }
+            case MY_PERMISSIONS_REQUEST_INTERNET:
             {
                 //check if the length of grantResults is greater than 0 and is equal to PERMISSION_GRANTED
                 if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED)
--- app/src/main/java/com/example/smspost/MyReceiver.java
+++ app/src/main/java/com/example/smspost/MyReceiver.java
@@ -7,6 +7,15 @@ import android.os.Build;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
 import android.widget.Toast;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class MyReceiver extends BroadcastReceiver {
 
@@ -48,6 +57,31 @@ public class MyReceiver extends BroadcastReceiver {
                     phoneNo = messages[i].getOriginatingAddress();
                 }
                 Toast.makeText(context, "Number: " +phoneNo+ "\nMessage: " +msg, Toast.LENGTH_LONG).show();
+
+                //Make a POST API call
+                RequestQueue MyRequestQueue = Volley.newRequestQueue(context);
+
+                //Create a String Request
+                String url = "http://yourdomain.com/path";
+                StringRequest MyStringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        //This code is executed if the server responds, whether or not the response contains data.
+                        //The String 'response' contains the server's response.
+                    }
+                }, new Response.ErrorListener() { //Create an error listener to handle errors appropriately.
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //This code is executed if there is an error.
+                    }
+                }) {
+                    protected Map<String, String> getParams() {
+                        Map<String, String> MyData = new HashMap<String, String>();
+                        MyData.put("Field", "Value"); //Add the data you'd like to send to the server.
+                        return MyData;
+                    }
+                };
+                MyRequestQueue.add(MyStringRequest);
             }
         }
     }
